23e58be60287ea3c8bdc84ca458839a55758edbe
Matt Gauger <matt.gauger@gmail.com>
2014-03-23 17:20:26 -0700

    Prepare to push a username-forked version of this lib to clojars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-mail "0.1.5"
+(defproject org.clojars.mathias/clojure-mail "0.2.0"
   :description "Clojure Email Library"
   :url "https://github.com/forward/clojure-mail"
   :license {:name "Eclipse Public License"
